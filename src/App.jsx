d081faa43d594b15da00079fd0ef24677e7b866b
Muhammed Adil <41191975+adilcpm@users.noreply.github.com>
2023-02-11 11:54:46 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <div className="App">
+      <div class="flex items-center justify-center h-screen w-screen bg-dark-square">
+        <div
+          class="md:ml-0 ml-[30px]
+      "
+        >
+          <h1 class="font-roboto font-thin italic text-7xl text-lime-200 drop-shadow-white">
+            Vayoo
+            <span class="text-gray-300 drop-shadow-none font-thin">
+              Markets
+            </span>
+            .
+          </h1>
+          <h1 class="ml-2 md:ml-3 mt-2 font-roboto font- italic text-2xl text-gray-400">
+            tradeFi synthetics
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'roboto': 'Roboto, sans-serif',
+    },
+    extend: {
+      backgroundImage: {
+        'dark-square': "url('./src/assets/repeated-square-dark-pattern.webp')",
+      }
+    },
+  },
+  plugins: [],
+}
