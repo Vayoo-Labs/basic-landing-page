017ce191136b570414073e0f4c8b02401b7f4e6e
Muhammed Adil <41191975+adilcpm@users.noreply.github.com>
2023-02-15 00:46:17 +0530

    add social logos

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,49 @@
 function App() {
   return (
     <div className="App">
-      <div class="flex flex-1 items-center justify-center h-screen w-screen bg-dark-square">
+      <div class="flex flex-col flex-1 items-center justify-center h-screen w-screen bg-dark-square">
         <div
           class="md:ml-0 ml-[30px]
       "
         >
           <h1 class="font-roboto font-thin italic text-7xl text-lime-200 drop-shadow-white">
-            Vayoo 
+            Vayoo
             <span class="break-normal text-gray-300 drop-shadow-none font-thin">
-              {' '}Markets
+              {" "}
+              Markets
             </span>
             .
           </h1>
           <h1 class="ml-2 md:ml-3 mt-2 font-roboto font- italic text-2xl text-gray-400">
             tradeFi synthetics
           </h1>
+
+          <div class="mt-4 flex items-center">
+            <div class="ml-3">
+              <a
+                class="hover:opacity-70 transition duration-200 ease-in-out"
+                href="https://twitter.com/VayooMarkets"
+              >
+                <img
+                  class="h-6"
+                  src="/src/assets/twitter-logo.svg"
+                  alt="Solana"
+                />
+              </a>
+            </div>
+            <div class="ml-3">
+              <a
+                class="hover:opacity-70 transition duration-200 ease-in-out"
+                href="#"
+              >
+                <img
+                  class="h-6"
+                  src="/src/assets/telegram-logo.svg"
+                  alt="Solana"
+                />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
